bcebca98903a48287a5b0ebc7d48ae1bdb1414ba
Mansur Sardauna <olaomansur@gmail.com>
2020-09-30 15:09:20 +0100

    completed shifter demo

---
+++ img/Shifter __ Css Ninja_files/main.js
@@ -0,0 +1,21 @@
+"use strict";
+
+initPageloader();
+
+$(document).ready(function(){
+
+    feather.replace();
+
+    initNavbar();
+
+    initReveals();
+
+    initCarousel();
+
+    initCounters();
+
+    initBackToTop();
+
+    initDarkTheme();
+
+})
